608c552af26a8e77e8a6efb899dbfea50716b0e1
Ezequiel Pereyra <50385003+Ezequielpereyraa@users.noreply.github.com>
2020-04-15 11:56:18 -0300

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,86 @@
+//Variables
+const listaTweet = document.querySelector('#lista-tweet');
+const formulario = document.querySelector('#formulario');
+//Evenlistener
+const funcGeneral = () =>{
+    formulario.addEventListener('submit', agregarTweet);
+    // Borrar 
+    listaTweet.addEventListener('click', borrarTweet)
+    // Contenido Cargado
+    document.addEventListener('DOMContentLoaded', localStorageListo);
+};
+
+// Funciones 
+const agregarTweet = (e)=>{
+    e.preventDefault();
+    if(tweet.value != ''){
+        const tweet = document.querySelector('#tweet').value;
+        const li = document.createElement('li');
+        li.className = 'pt-4 w-50'
+        li.innerHTML = tweet
+        listaTweet.append(li);
+        const buttonBorrar = document.createElement('button');
+        buttonBorrar.className = 'borrar-tweet float-right';
+        buttonBorrar.innerText = 'x'
+        li.append(buttonBorrar);
+        agregarLocalStorage(tweet)
+    } else{
+       alert('Escribir Tweet')
+    }
+  
+    formulario.reset()
+   
+}
+// borrar el tweet
+function borrarTweet(e) {
+    e.preventDefault()
+    //console.log('diste click en la lista')
+    if(e.target.className === 'borrar-tweet') {
+        e.target.parentElement.remove()
+        borrarTweetLocalStorage(e.target.parentElement.innerText)
+        //console.log(e.target.parentElement.innerText)
+    } 
+ 
+}
+   
+// Agregar el tweets al LocalStorage
+const agregarLocalStorage = (tweet)=>{
+   let tweets;
+   tweets = obtenerLocalStorage();
+   //  añadir el tweet
+   tweets.push(tweet);
+   // Arreglo para el localstorage
+   localStorage.setItem('tweets',JSON.stringify(tweets))
+}
+// Obtenener tweets LocalStorage 
+const obtenerLocalStorage = ()=>{
+    let tweets
+    // Revisar si hay algo en el local 
+    if(localStorage.getItem('tweets') === null){
+        tweets = []
+    } else {
+        tweets = JSON.parse(localStorage.getItem('tweets'));
+    }
+    return tweets;
+}
+// Cargar los tweets del LocalStorage en el dom 
+const localStorageListo = ()=>{
+    let tweets;
+    tweets = obtenerLocalStorage();
+    tweets.forEach(tweet => {
+    const li = document.createElement('li');
+    li.className = 'pt-4 w-50'
+    li.innerHTML = tweet
+    listaTweet.append(li);
+    const buttonBorrar = document.createElement('button');
+    buttonBorrar.className = 'borrar-tweet float-right';
+    buttonBorrar.innerText = 'x'
+    li.append(buttonBorrar);
+   });
+}
+const borrarTweetsLocalStorage = ()=>{
+    let tweets;
+ 
+}
+ 
+funcGeneral()
